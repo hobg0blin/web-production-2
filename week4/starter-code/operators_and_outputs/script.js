0078452f404ee4fd0c4a7d4a4ffb8a52baf5092b
Brent Bailey <bb2906@nyu.edu>
2021-09-21 13:38:12 -0400

    more week 4

--- week4/starter-code/operators_and_outputs/script.js
+++ week4/starter-code/operators_and_outputs/script.js
@@ -1,54 +1,80 @@
-// Operators and Outputs
-  let total = 0 // Global variable to hold the total
-  let resultElement = O("resultParagraph") // we use this a lot, so might as well make it a global variable
-    function getTotal() {
-      let valueElement = O("valueText")
-      // get value of input element
-      let valueText = valueElement.value
-      // casting to a number - even though we set our input element to be a number, it still comes out as a string in JavaScript
-      let value = parseInt(valueText)
-      return value
-    }
+//Global variables
+const media = document.querySelector('video') // this just gets the video element
+let playbackSpeed = 1 // Global variable to hold the playbackSpeed
+
+function getModifier() {
+  let valueElement = O("valueText")
+  // get value of input element
+  let valueText = valueElement.value
+  // casting to a number - even though we set our input element to be a number, it still comes out as a string in JavaScript
+  let value = Number(valueText)
+  return value
+}
 
     /* This function reads the value from the valueText element
-       and adds it to the global total value */
-    function addToTotal() {
-      let value = getTotal()
+       and adds it to the global playback speed value */
+function addToModifier() {
+  let value = getModifier()
+  // Set the playback speed
+  media.playbackRate = playbackSpeed
+  // Display the updated playback speed
+  let resultElement = O("playbackSpeed")
+  let resultString = "Playback speed is " + playbackSpeed
+  resultElement.innerText = resultString
+}
+function subtractModifier() {
+  let value = getModifier()
+  // Set the playback speed
+  media.playbackRate = playbackSpeed
+  // Display the updated playback Speed
+  let resultElement = O("playbackSpeed")
+  let resultString = "Playback speed is " + playbackSpeed
+  resultElement.innerText = resultString
+}
 
-      // Display the updated total
-      let resultString = "Total is " + total
-      resultElement.innerText = resultString
-    }
-    function subtractTotal() {
-      let value = getTotal()
-      // Display the updated total
-      let resultString = "Total is " + total
-      resultElement.innerText = resultString
-    }
+function multiplyModifier() {
+  let value = getModifier()
+  // Set the playback speed
+  media.playbackRate = playbackSpeed
 
-  function multiplyTotal() {
-      let value = getTotal()
-      // Display the updated total
-      let resultString = "Total is " + total
-      resultElement.innerText = resultString
-    }
+  // Display the updated playback speed
+  let resultElement = O("playbackSpeed")
+  let resultString = "Playback speed is " + playbackSpeed
+  resultElement.innerText = resultString
+}
 
-  function divideTotal() {
-      let value = getTotal()
-      // Display the updated total
-      let resultString = "Total is " + total
-      resultElement.innerText = resultString
-    }
+function divideModifier() {
+  let value = getModifier()
+  // Set the playback speed
+  media.playbackRate = playbackSpeed
+
+  // Display the updated playback seed
+  let resultElement = O("playbackSpeed")
+  let resultString = "Playback speed is " + playbackSpeed
+  resultElement.innerText = resultString
+}
 
 
 
-    /* This function clears the total value and updates the display
-    */
-    function zeroTotal() {
-      total = 0; // set the global total to 0
+  /* This function clears the modifier value and updates the display
+  */
+function zeroModifier() {
+  playbackSpeed = 1; // set the global modifier to 0
+  // Set the playback speed
+  media.playbackRate = playbackSpeed
 
-      // update the display
-      resultElement.innerText = "Total is 0"
-    }
+  // update the display
+  let resultElement = O("playbackSpeed")
+  resultElement.innerText = "Playback speed is 1"
+}
 
+const play = O('play')
+
+function playPauseMedia() {
+  if(media.paused) {
+    media.play();
+  } else {
+    media.pause();
+  }
+}
 
